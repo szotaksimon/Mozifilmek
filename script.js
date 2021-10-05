1298f093e6a6a55b4eda3a221513b651ae53058d
szotaksimon <szotak.simon16@gmail.com>
2021-10-05 14:41:34 +0200

    function hozzáad létrehozása

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 class Film{
-    #nev;
-    #rendezo;
-    #kiad_ev;
 
     constructor (nev, rendezo, kiad_ev){
         this.nev = nev;
         this.rendezo = rendezo;
-        this.#kiad_ev = kiad_ev;
+        this.kiad_ev = kiad_ev;
     }
 
 }
 
 let adatok = [];
+
+function hozzaAd(){
+    let nev = document.getElementById("nev").value;
+    let rendezo = document.getElementById("rendezo").value;
+    let kiad_ev = document.getElementById("kiad_ev").value;
+
+    let obj = new Film(nev, rendezo, kiad_ev);
+    adatok.push(obj);
+
+    document.getElementById("nev").value = "";
+    document.getElementById("rendezo").value = "";
+    document.getElementById("kiad_ev").value = "";
+
+
+}
