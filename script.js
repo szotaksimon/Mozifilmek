4b27541b7046c97846d4103eaf374405bb7eb5e5
szotaksimon <szotak.simon16@gmail.com>
2021-10-05 14:14:32 +0200

    Film osztály és üres tömb létrehozása

---
+++ script.js
@@ -0,0 +1,14 @@
+class Film{
+    #nev;
+    #rendezo;
+    #kiad_ev;
+
+    constructor (nev, rendezo, kiad_ev){
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.#kiad_ev = kiad_ev;
+    }
+
+}
+
+let adatok = [];
